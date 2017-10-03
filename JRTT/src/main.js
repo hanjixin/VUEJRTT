fb5db97408dace3950af837f49bd86af8f194923
““韩吉鑫” <764671440@qq.com>
2017-10-03 16:18:59 +0800

    first commit

---
+++ JRTT/src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import style from  '@/assets/css/style.css'
+
+import Vueaxios from  "vue-axios"
+import axios from  "axios"
+import infiniteScroll from 'vue-infinite-scroll'
+Vue.use(infiniteScroll)
+Vue.use(Vueaxios,axios);
+Vue.use(style);
+Vue.use(window)
+axios.defaults.baseURL ='http://localhost:8080/api';
+Vue.config.productionTip = false
+Vue.prototype.infoID="";
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
---
+++ JRTT/src/router/index.js
@@ -0,0 +1,120 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Tuijian from '@/components/tuijian'
+import index from '@/views/index'
+import detail from '@/components/detail'
+import Shipin from '@/components/shipin'
+import Rendian from '@/components/redian'
+import Shehui from '@/components/shehui'
+import Yule from '@/components/yule'
+import Junshi from '@/components/junshi'
+import Keji from '@/components/keji'
+import Qiche from '@/components/qiche'
+import Tiyu from '@/components/tiyu'
+import Caijing from '@/components/Caijing'
+import Guoji from '@/components/guoji'
+import Shishang from '@/components/shishang'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: index,
+      children:[{
+        path: '/tuijian',
+        name: 'tuijian',
+        component:Tuijian
+      }
+      ,{
+          path: '/',
+          name: 'tuijian',
+          component:Tuijian,
+
+        },
+        {
+          path: '/shipin',
+          name: 'shipin',
+          component:Shipin,
+
+        },
+
+        {
+          path: '/redian',
+          name: 'redian',
+          component:Rendian,
+
+        }
+        ,
+
+        {
+          path: '/shehui',
+          name: 'shehui',
+          component:Shehui,
+
+        }
+        ,
+
+        {
+          path: '/yule',
+          name: 'yule',
+          component:Yule,
+
+        },
+        ,
+        {
+          path: '/junshi',
+          name: 'junshi',
+          component:Junshi,
+
+        } ,
+        {
+          path: '/keji',
+          name: 'keji',
+          component:Keji,
+
+        } ,
+        {
+          path: '/qiche',
+          name: 'qiche',
+          component:Qiche,
+
+        }
+        ,
+        {
+          path: '/tiyu',
+          name: 'tiyu',
+          component:Tiyu,
+
+        }
+        ,
+        {
+          path: '/caijing',
+          name: 'caijing',
+          component:Caijing,
+
+        },
+        {
+          path: '/guoji',
+          name: 'guoji',
+          component:Guoji,
+
+        },
+        {
+          path: '/shishang',
+          name: 'shishang',
+          component:Shishang,
+
+        }
+        ]
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      component:detail
+    }
+  ]
+})
